6bfec352a61bd651853c6dcf77bbaee4a46bc598
zdzDesigner <1696498456@qq.com>
2025-02-11 12:03:21 +0800

    ~

--- cameras/main.go
+++ cameras/main.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	// "path/filepath"
-	"strings"
+	"path/filepath"
+	// "strings"
 
 	"cameras/src/config"
 
@@ -52,25 +52,28 @@ func convStream() {
 	os.MkdirAll(hls_dir, 0o755)
 
 	// 启动FFmpeg转码进程
-	// cmd := exec.Command("ffmpeg",
-	// 	// "-rtsp_transport", "tcp", // 强制TCP传输
-	// 	"-i", rtsp_url, // 输入源
-	// 	"-c:v", "libx264", // 视频编码
-	// 	"-crf", "23", // 质量参数
-	// 	"-preset", "veryfast", // 编码速度
-	// 	"-g", fmt.Sprintf("%d", hls_segment*2), // GOP大小
-	// 	"-f", "hls", // 输出格式
-	// 	"-hls_time", fmt.Sprintf("%d", hls_segment),
-	// 	"-hls_list_size", "6", // 播放列表保留切片数
-	// 	"-hls_flags", "delete_segments", // 自动删除旧切片
-	// 	filepath.Join(hls_dir, hls_playlist),
-	// )
-	ffmpeg_args := "-i rtsp://localhost:8554/mystream -c:v copy -c:a aac -f hls -hls_time 2 -hls_list_size 5 -hls_wrap 5 ./static/hls/output.m3u8"
-	cmd := exec.Command("ffmpeg", strings.Split(ffmpeg_args, " ")...)
+	cmd := exec.Command("ffmpeg",
+		// "-rtsp_transport", "tcp", // 强制TCP传输
+		"-i", rtsp_url, // 输入源
+		"-c:v", "libx264", // 视频编码
+		"-crf", "23", // 质量参数
+		// "-preset", "veryfast", // 编码速度
+		"-preset", "ultrafast", // 最快编码速度
+		"-g", fmt.Sprintf("%d", hls_segment*2), // GOP大小
+		"-f", "hls", // 输出格式
+		"-hls_time", fmt.Sprintf("%d", hls_segment),
+		"-hls_list_size", "6", // 播放列表保留切片数
+		// "-hls_flags", "delete_segments", // 自动删除旧切片
+		"-hls_flags", "delete_segments+append_list", // 实时更新播放列表
+		"-tune", "zerolatency", // 零延迟编码
+		filepath.Join(hls_dir, hls_playlist),
+	)
+	// ffmpeg_args := "-i rtsp://localhost:8554/mystream -c:v copy -c:a aac -f hls -hls_time 2 -hls_list_size 5 -hls_wrap 5 ./static/hls/output.m3u8"
+	// cmd := exec.Command("ffmpeg", strings.Split(ffmpeg_args, " ")...)
 
 	// 捕获FFmpeg日志
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
 
 	// 启动转码
 	if err := cmd.Start(); err != nil {
