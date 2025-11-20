7626d31e351820a1cd43fdc79b1fe4b0e0786151
zdzDesigner <1696498456@qq.com>
2025-11-20 17:01:14 +0800

    ~

--- cgo/ffmpeg/run.dev.sh
+++ cgo/ffmpeg/run.dev.sh
@@ -20,10 +20,7 @@ export LD_LIBRARY_PATH="$FFMPEG_LIB/lib:$LD_LIBRARY_PATH"  # 运行时库路径
 # go run .
 # go run . -o output.wav /home/zdz/Documents/Try/Python/course/http-server/go_client/server/output/1_019a8907.wav /home/zdz/Documents/Try/Python/course/http-server/go_client/server/output/1_01bd5489.wav
 # go run . -o output.wav /home/zdz/Documents/Try/Python/course/http-server/go_client/server/output/1_019a8907.wav /home/zdz/Documents/Try/Python/course/http-server/go_client/server/output/1_01bd5489.wav
-
 # go run . -o output.wav /home/zdz/Downloads/capgen_example.wav  /home/zdz/Downloads/capgen_example.wav
-# cp ./lib/examples/joint/main.go  ./main.go
-# cp ./main.go ./lib/examples/joint/main.go
 
 go run . -o output.wav ./assets/capgen_example.wav  ./assets/aigei_com.wav
 
@@ -36,7 +33,7 @@ go run . -o output.wav ./assets/capgen_example.wav  ./assets/aigei_com.wav
 # ffmpeg -i ./assets/capgen_example.wav -i ./assets/aigei_com.wav -filter_complex "[0:a][1:a]concat=n=2:v=0:a=1[a];[a]aformat=channel_layouts=mono[outa]" -map "[outa]" output_ff.wav
 
 
-ffmpeg -i ./assets/capgen_example.wav -i ./assets/aigei_com.wav \
--filter_complex "[0:a][1:a]concat=n=2:v=0:a=1[a];[a]aformat=channel_layouts=mono[outa]" \
--map "[outa]" output_ff.wav
+# ffmpeg -i ./assets/capgen_example.wav -i ./assets/aigei_com.wav \
+# -filter_complex "[0:a][1:a]concat=n=2:v=0:a=1[a];[a]aformat=channel_layouts=mono[outa]" \
+# -map "[outa]" output_ff.wav
 
